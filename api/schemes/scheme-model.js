9f5afe337dc6a90c709bfcda16156947ba132f67
taylersala <taylersala@gmail.com>
2024-08-09 17:38:47 -0700

    1

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -26,7 +26,7 @@ function find() { // EXERCISE A
 
 }
 
-function findById(scheme_id) { // EXERCISE B
+ async function findById(scheme_id) { // EXERCISE B
   /*
     1B- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`:
 
@@ -92,6 +92,27 @@ function findById(scheme_id) { // EXERCISE B
         "steps": []
       }
   */
+  const rows = await db('schemes as sc')
+    .leftJoin('steps as st', 'sc.scheme_id', '=', 'st.scheme_id')
+    .where('sc.scheme_id', scheme_id)
+    .select('st.*', 'sc.scheme_name', 'sc.scheme_id')
+    .orderBy('st.step_number')
+
+    const result = {
+      scheme_id: rows[0].scheme_id,
+      scheme_name: rows[0].scheme_name,
+      steps: []
+    }
+    rows.forEach(row => {
+      if(row.step_id) {
+        result.steps.push({
+          scheme_id: row.scheme_id,
+          scheme_name: row.scheme_name,
+          instructions: row.instructions,
+        })
+      }
+    })
+    return result
 }
 
 function findSteps(scheme_id) { // EXERCISE C
